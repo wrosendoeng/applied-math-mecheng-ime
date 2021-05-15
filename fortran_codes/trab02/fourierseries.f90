023cbbd92e85c1b290559f7a0e65dd0b29f9d3a2
wrosendoeng <wrosendoeng@gmail.com>
2021-05-15 15:14:43 -0300

    new Makefile

--- fortran_codes/trab02/fourierseries.f90
+++ fortran_codes/trab02/fourierseries.f90
@@ -3,19 +3,21 @@ module fourierseries
     implicit none
     contains
 
-    function fouriertransformation(x,m,n) result(fft)
+    function fouriertransformation(x, t, alpha, bar_length, m, n) result(fft)
         integer(fgsl_int) :: i, k, n, m
-        real(fgsl_double) :: cm(m), sin_exp(m,n)
-        real(fgsl_double), intent(in) :: x(n), t(n)
+        real(fgsl_double) :: cm(m), sin_fun(m,n), exp_tim(m,n)
+        real(fgsl_double), intent(in) :: x(n), t(n), alpha, bar_length
+        real(fgsl_double)             :: fft(n)
 
         do k = 1, m
-            cm(k) = 2.0_fgsl_double*bar_length/(k*m_pi)**2*(2*sin(k*m_pi/2.0) - (k*m_pi)*cos(k*m_pi*/2.0))
+            cm(k) = 2.0_fgsl_double*bar_length*(2*sin(k*m_pi/2.0) - (k*m_pi)*cos(k*m_pi/2.0))/(k*m_pi)**2
             do i = 1, n
-                sin_exp(k,i) = sin(k*m_pi*x(i)/bar_length)*exp(-t(i)*(alpha*k*m_pi/bar_length)**2)
+                sin_fun(k,i) = sin(k*m_pi*x(i)/bar_length)
+		exp_tim(k,i) = 1.0_fgsl_double/exp(t(i)*(alpha*k*m_pi/bar_length)**2)
             end do
         end do 
     
-        fft = matmul(bm,sin_exp)
+        fft = matmul(cm,sin_fun*exp_tim)
 
     end function  
 end module fourierseries
--- fortran_codes/trab02/main.f90
+++ fortran_codes/trab02/main.f90
@@ -17,9 +17,9 @@ program main
     ! Defining a name for Fourier Analysis Exercise
     character(kind=fgsl_char,len=6) :: file_name = "result"
     character(kind=fgsl_char,len=6) :: unit1,arg1	
-    integer(fgsl_int) :: iter=1,nmax=1000,mmax,new_unit,period
-    real(fgsl_double), parameter :: bar_length = 2.0_fgsl_double, end_time = 10.0_fgsl_double ! Length in meters
-    real(fgsl_double) :: dxvec, start, finish
+    integer(fgsl_int) :: iter=1,nmax=1000,mmax,new_unit1,period
+    real(fgsl_double), parameter :: bar_length = 2.0e3, end_time = 10.0_fgsl_double ! Length in meters
+    real(fgsl_double) :: dxvec, dtvec, start, finish, thermal_diff = 13.5865297
     real(fgsl_double), allocatable :: xvec(:), tvec(:), yvec(:), fft_res(:)
     
     ! Command-line input of how many iterations has to be done in Fourier's Series
@@ -28,8 +28,7 @@ program main
     read(arg1,*) mmax ! Transforming string into an integer
 
     write(unit1,'(I6.6)') mmax ! Creating a new string with 6 digits to store results
-    open(newunit=new_unit1,file=file_name//"_"//trim(unit1)//".txt",status="replace",action="write")
-    open(newunit=new_unit2,file=file_name//"_"//trim(unit1)//".txt",status="replace",action="write")
+    open(newunit=new_unit1,file=file_name//"_"//trim(arg1)//".txt",status="replace",action="write")
     
     allocate(xvec(nmax))
     allocate(tvec(nmax))
@@ -50,12 +49,12 @@ program main
     end do
     
     ! Fourier Series Approximation
-    fft_res = fouriertransformation(xvec,tvec,mmax,nmax) 
+    fft_res = fouriertransformation(xvec,tvec, thermal_diff, bar_length, mmax,nmax) 
 
     ! Creating a text file with results of f(x) and Fourier Series approximation
     ! write(new_unit,'(a11,a6,a11,a6,a11)') &
     ! & "x"," ","f(x)"," ","fft(x)"    
-    write(new_unit,'(f11.8,a6,a11,a6,f11.8)') &
+    write(new_unit1,'(f11.8,a10,f11.8,a10,f11.8)') &
     & (xvec(iter)," ",tvec(iter)," ",fft_res(iter),iter=1,nmax)
     
     deallocate(xvec)
@@ -63,9 +62,9 @@ program main
     deallocate(fft_res)
 
     ! Plotting chart with interface between Gnuplot and Fortran
-    call execute_command_line("gnuplot -c plot.plt " &
-    & //file_name//"_"//trim(unit1)//".txt "//arg1) ! preparing a command-line for gnuplot
+    call execute_command_line("gnuplot -c animation.plt " &
+    & //file_name//"_"//trim(arg1)//".txt") ! preparing a command-line for gnuplot
 
     call cpu_time(finish)
-    print *, "CPU time spent in code with "//arg1//" parcels: ", finish-start
-end program main2
+    print *, "CPU time spent in code with "//trim(arg1)//" parcels: ", finish-start, " seconds."
+end program main
